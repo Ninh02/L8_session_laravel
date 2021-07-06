56ec7c85a212ae4f105b558fe1b62c981a220237
Ninh02 <ninhdo10021998@gmail.com>
2021-07-06 08:29:33 +0700

    thuc hanh session

---
+++ TH_luu_ten_nguoi_dung/web_using_session/resources/views/login.blade.php
@@ -0,0 +1,12 @@
+@extends('layout.master')
+@section('content')
+    <div class="title m-b-md">
+        Laravel Session
+    </div>
+
+    <div class="links">
+        <a href="{{ route('user.login') }}">
+            <button type="button" class="btn btn-outline-primary">Đăng Nhập</button>
+        </a>
+    </div>
+@endsection
